ae7435a904e7e0e8ffc8818796eeeeddc006d58d
AMIR <you@example.com>
2025-08-29 22:49:42 +0200

    crud

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from fastapi import FastAPI
+
+
+from schemas import NewProduct, SavedProduct
+
+from base_storage import storage
+
+
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return  {'status':'ok'}
+
+@app.post("/books/", tags=["Книги"])
+def create_book(new_book: NewProduct) -> SavedProduct:
+    product = storage.create_product(new_book)
+    return product
+
+
+@app.get("/books/{book_id}")
+def get_book(book_id: str) -> SavedProduct:
+    book = storage.get_product(book_id)
+    return book
+
+
+@app.get("/books/")
+def get_books(query: str = "", limit: int = 10, skip: int = 0) -> list[SavedProduct]:
+    books = storage.get_products(q=query, limit=limit, skip=skip)
+    return books
+
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: str) -> dict:
+    storage.delete_product(book_id)
+    return {}
